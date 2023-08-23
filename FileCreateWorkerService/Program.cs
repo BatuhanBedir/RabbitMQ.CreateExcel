0a586883f3d909b5c6dbe9a3174726afb4a3339a
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-23 21:59:45 +0300

    FilesCont.UploadMethod

--- FileCreateWorkerService/Program.cs
+++ FileCreateWorkerService/Program.cs
@@ -1,11 +1,18 @@
 using FileCreateWorkerService;
+using FileCreateWorkerService.Models;
 using FileCreateWorkerService.Services;
+using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
         IConfiguration Configuration = hostContext.Configuration;
+
+        services.AddDbContext<AdventureWorks2019Context>(opt =>
+        {
+            opt.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+        });
 
         services.AddSingleton<RabbitMQClientService>();
 
